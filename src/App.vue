404f393ac27c526f87734c81dde0b16a483e5860
Amiltonxavier <amiltonxavier1999@gmail.com>
2025-05-20 15:56:04 +0100

    Event Handling

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <script setup>
 import { ref } from "vue";
 import socksGreenImage from "./assets/images/socks_green.jpeg";
+import socksBlueImage from "./assets/images/socks_blue.jpeg";
 const product = ref("Socks");
+const details = ref(["50% cotton", "30% wool", "20% polyester"]);
 const image = ref(socksGreenImage);
+const inventory = 11;
+
+const variants = ref([
+	{ id: 2234, color: "green", image: socksGreenImage },
+	{ id: 2235, color: "blue", image: socksBlueImage },
+]);
+
+const cart = ref(0);
+
+const addToCard = () => {
+	cart.value += 1;
+};
+
+const updateImage = (variantImage) => {
+	image.value = variantImage;
+};
 </script>
 
 <template>
   <div class="nav-bar"></div>
+  <div class="cart">Cart{{ cart }}</div>
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
@@ -14,6 +33,18 @@ const image = ref(socksGreenImage);
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <p v-if="inventory > 10">In Stock</p>
+        <p v-else-if="inventory <= 10 && inventory > 0">Almost Sold out</p>
+          <p v-else>Out of Stock</p>
+
+          <ul>
+              <li v-for="detail in details">{{ detail }}</li>
+          </ul>
+
+          <div v-for="variant in variants" :key="variant.id" @mouseover="updateImage(variant.image)">
+                {{ variant.color }}
+          </div>
+          <button class="button" v-on:click="addToCard">Add to Card</button>
       </div>
     </div>
   </div>
