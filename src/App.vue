c3dd95944e13df83741954bec333f0b5b915b30b
Amiltonxavier <amiltonxavier1999@gmail.com>
2025-05-20 15:03:21 +0100

    feat: setup & binding

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue";
+import socksGreenImage from "./assets/images/socks_green.jpeg";
+const product = ref("Socks");
+const image = ref(socksGreenImage);
+</script>
+
+<template>
+  <div class="nav-bar"></div>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img :src="image">
+      </div>
+      <div class="product-info">
+        <h1>{{ product }}</h1>
+      </div>
+    </div>
+  </div>
+
+</template>
+
